87cdb24fc7814b35383bb8c6d76427ddf93f738c
rocky zhou <rocky_zhou@qq.com>
2018-08-17 15:23:38 +0800

    短信发送

---
+++ src/main/java/com/ruiliang/appsrv/service/SmsService.java
@@ -0,0 +1,18 @@
+package com.ruiliang.appsrv.service;
+
+import com.ruiliang.appsrv.exception.SmsSendFailureException;
+
+public interface SmsService {
+
+	int SMS_TYPE_RESET_PASSWD = 1;
+
+	/**
+	 * 发送短信
+	 * 
+	 * @param type    发送验证码类型 1 密码重置
+	 * @param dest    接收短信目的号码
+	 * @param content 短信内容
+	 * @throws SmsSendFailureException
+	 */
+	void send(int type, String dest, String content) throws SmsSendFailureException;
+}
