34a7fe42777c191df7b5dc3355247a9eca71f2e8
rocky zhou <rocky_zhou@qq.com>
2018-08-20 17:51:11 +0800

    修改短信发送

--- src/main/java/com/ruiliang/appsrv/service/impl/TencentCloudSmsServiceImpl.java
+++ src/main/java/com/ruiliang/appsrv/service/impl/TencentCloudSmsServiceImpl.java
@@ -2,12 +2,11 @@ package com.ruiliang.appsrv.service.impl;
 
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.github.qcloudsms.SmsSingleSender;
-import com.github.qcloudsms.SmsSingleSenderResult;
 import com.jfinal.kit.PropKit;
+import com.qcloud.sms.SmsSingleSender;
+import com.qcloud.sms.SmsSingleSenderResult;
 import com.ruiliang.appsrv.exception.SmsSendFailureException;
 import com.ruiliang.appsrv.service.SmsService;
 
@@ -16,13 +15,16 @@ public class TencentCloudSmsServiceImpl implements SmsService {
 
 	@Override
 	public void send(int type, String dest, String content) throws SmsSendFailureException {
-		PropKit.use("application.properties");
-		SmsSingleSender singleSender = new SmsSingleSender(Integer.parseInt(PropKit.get("tencloud.sms.appid")), PropKit.get("tencloud.sms.appkey"));
-		SmsSingleSenderResult singleSenderResult = null;
-		int tmplId = 177653;
-		ArrayList<String> params = new ArrayList<>();
-		params.add(content);
+
 		try {
+			PropKit.use("application.properties");
+			SmsSingleSender singleSender = new SmsSingleSender(Integer.parseInt(PropKit.get("tencloud.sms.appid")),
+					PropKit.get("tencloud.sms.appkey"));
+			SmsSingleSenderResult singleSenderResult = null;
+			int tmplId = 177653;
+			ArrayList<String> params = new ArrayList<>();
+			params.add(content);
+
 			singleSenderResult = singleSender.sendWithParam("86", dest, tmplId, params, "", "", "");
 		} catch (Exception e) {
 			throw new SmsSendFailureException("短信发送出错: " + e.toString());
---
+++ src/test/java/com/ruiliang/appsrv/test/service/TestTencentCloudSmsService.java
@@ -0,0 +1,36 @@
+package com.ruiliang.appsrv.test.service;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ruiliang.appsrv.exception.SmsSendFailureException;
+import com.ruiliang.appsrv.service.SmsService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestTencentCloudSmsService {
+
+	@Autowired
+	@Qualifier("tencentSmsService")
+	private SmsService smsService;
+
+	@Test
+	public void testSendCode() {
+		try {
+			JSONObject json = new JSONObject();
+			json.put("name", "rocky");
+			System.out.println(json.toString());
+
+			smsService.send(SmsService.SMS_TYPE_RESET_PASSWD, "18575588430", "332211");
+		} catch (SmsSendFailureException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
