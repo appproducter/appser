abbfe7c0f92def19e6daf6caf984154b4405b5f5
rocky zhou <rocky_zhou@qq.com>
2018-08-20 14:01:16 +0800

    1

---
+++ src/test/java/com/ruiliang/appsrv/test/StringTest.java
@@ -0,0 +1,10 @@
+package com.ruiliang.appsrv.test;
+
+import com.ruiliang.appsrv.util.RandomUtil;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		System.out.println(RandomUtil.getRandomString(12));
+	}
+}
