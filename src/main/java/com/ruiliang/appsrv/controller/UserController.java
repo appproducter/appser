6f8d49d2fe5e9d36cdfc49dc8bcafb0a2ec092eb
zz <526380134@qq.com>
2018-08-21 17:50:22 +0800

    0

--- src/main/java/com/ruiliang/appsrv/controller/UserController.java
+++ src/main/java/com/ruiliang/appsrv/controller/UserController.java
@@ -1,32 +1,68 @@
 package com.ruiliang.appsrv.controller;
 
-import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import sun.misc.BASE64Decoder;
+
 import com.alibaba.fastjson.JSONObject;
-import com.ruiliang.appsrv.util.UploadUtil;
+import com.ruiliang.appsrv.pojo.UserInfo;
+import com.ruiliang.appsrv.pojo.UserToken;
+import com.ruiliang.appsrv.service.UserInfoService;
+import com.ruiliang.appsrv.service.UserTokenService;
+import com.ruiliang.appsrv.util.CharUtils;
+import com.ruiliang.appsrv.util.DateUtil;
 
 /**
  * @author LinJian.Liu
  *
+ * 设置头像
  */
 @RestController
 @RequestMapping("api/user")
 public class UserController {
 
+	private static final String dateTimePath = DateUtil.getFormatDateString(new Date(), "yyyyMMdd") ;
+	
+	/***上传图片的保存地址***/
+	//private static final String storagePath = "/images/user_img" ;
+	private static final String storagePath = "E:/img" ;
+	/***上传图片的保存地址***/
+	//private static final String uploadPath = "/data"+ storagePath +"/"+ dateTimePath + "/" ;
+	private static final String uploadPath =  storagePath +"/"+ dateTimePath + "/" ;
+	/**支持上传的图片格式****/
+	private static final String[] imgSuffix = {"gif","jpg","png","jpeg","bmp"} ;
+	/***支持图片的最大大小***/
+	private static long maxSize = 2048;  //2M
+	
 	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 	
+	@Value("${img}")
+	private String img;
+	
+	@Value("${appurl}")
+	private String appurl;
+	
+	@Autowired
+	private UserInfoService uService;
+	
+	@Autowired
+	private UserTokenService utService;
+	
 	@RequestMapping("setavatar")
-	@Deprecated
 	public JSONObject setavatar(HttpServletRequest request){
-		JSONObject obj = UploadUtil.uploadImg(request) ;
 		JSONObject reslut = new JSONObject();
 		JSONObject data = new JSONObject();
 		
@@ -45,33 +81,73 @@ public class UserController {
 			reslut.put("data", data);
 			return reslut;
 		}
-		//查看用户是否存在
-		String imgUrl = null;
+		UserToken userToken = utService.findByToken(token);
+		if(null == userToken){
+			reslut.put("state", -1);
+			reslut.put("msg", "无效的token");
+			reslut.put("data", data);
+			return reslut;
+		}
+		UserInfo userInfo = uService.selectUserInfoByUid(userToken.getuId());
 		
-		//不存在
+		if(null == userInfo){
+			reslut.put("state", -1);
+			reslut.put("msg", "用户不存在");
+			reslut.put("data", data);
+			return reslut;
+		}
 		
-		
-		if(obj == null){
-			data.put("reslut", -1);
-			data.put("msg", "fail");
+		//获取图片字符串
+		String imgString = JSONObject.parseObject(da).getString("avatar");
+		//图片后缀
+		String suffix = JSONObject.parseObject(da).getString("suffix");
+		if(!CharUtils.contains(imgSuffix, suffix)){
+			reslut.put("state", -1);
+			reslut.put("msg", "图像格式错误");
+			reslut.put("data", data);
+			return reslut;
+		}
+		if(null == imgString){
+			reslut.put("state", -1);
+			reslut.put("msg", "图像为空");
+			reslut.put("data", data);
+			return reslut;
+		}
+		String imgurl = uploadPath+userInfo.getUId()+"."+suffix;
+	    BASE64Decoder decoder = new BASE64Decoder();
+	    byte[] b;
+		try {
+			b = decoder.decodeBuffer(imgString);
+			long size=(long)b.length/1024;
+			if(size > maxSize){
+				reslut.put("state", -1);
+				reslut.put("msg", "图像大小超过限制");
+				reslut.put("data", data);
+				return reslut;
+		    }
+			for(int i = 0; i < b.length; ++i) {
+			       if (b[i] < 0) {
+			             b[i] += 256;
+			         }
+			    
+			    OutputStream out = new FileOutputStream(imgurl);
+			    out.write(b);
+			    out.flush();
+			    out.close();
+			    
+			}
+		} catch (IOException e) {
 			reslut.put("state", -1);
 			reslut.put("msg", "图像上传失败");
 			reslut.put("data", data);
 			return reslut;
 		}
-		int error = obj.getIntValue("state") ;
-		if(error!=0){
-			data.put("reslut", -1);
-			data.put("msg", "fail");
-			reslut.put("state", -1);
-			reslut.put("msg", "图像上传失败");
-			reslut.put("data", data);
-			return reslut;
-		}
-		imgUrl = obj.getString("url") ;
-		System.out.println("vvvvvvvvv"+imgUrl);
-		return null;
-		
+		data.put("imgurl", appurl+"/"+imgurl);
+		data.put("result", 0);
+		data.put("msg", "success");
+		reslut.put("state", 0);
+		reslut.put("msg", "图像更新成功");
+		reslut.put("data", data);
+		return reslut;
 	}
-	
 }
--- src/main/java/com/ruiliang/util/SignUtil.java
+++ src/main/java/com/ruiliang/util/SignUtil.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
