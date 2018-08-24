ce02d9b93a42c3a98f1132c37b1ba616974785a0
rocky zhou <rocky_zhou@qq.com>
2018-08-24 11:21:23 +0800

    1

--- src/main/java/com/ruiliang/appsrv/controller/GroupCreateController.java
+++ src/main/java/com/ruiliang/appsrv/controller/GroupCreateController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
