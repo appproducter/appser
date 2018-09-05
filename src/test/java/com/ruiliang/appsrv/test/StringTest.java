d23df764a7b53214de995b1d3c06719ae82b7878
rocky zhou <rocky_zhou@qq.com>
2018-09-05 14:07:03 +0800

    ;

--- src/test/java/com/ruiliang/appsrv/test/StringTest.java
+++ src/test/java/com/ruiliang/appsrv/test/StringTest.java
@@ -1,10 +1,13 @@
 package com.ruiliang.appsrv.test;
 
+import com.ruiliang.appsrv.util.MD5Util;
 import com.ruiliang.appsrv.util.RandomUtil;
 
 public class StringTest {
 
 	public static void main(String[] args) {
 		System.out.println(RandomUtil.getRandomString(12));
+		
+		System.out.println(MD5Util.MD5Encode("123456"));
 	}
 }
